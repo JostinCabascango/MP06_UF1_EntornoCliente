dfd19eeabb7833c4d899cfc7773dceee313be5e9
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-04 19:09:28 +0200

    Refactorizar function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function decimalTo(decimal, base) {
+function decimalToBase(decimal, base) {
     let hexadecimal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
     let numeroDecimal = parseInt(decimal);
     let resultado = "";
@@ -6,15 +6,15 @@ function decimalTo(decimal, base) {
     while (numeroDecimal > 0) {
         let modulo = numeroDecimal % base;
         resultado = hexadecimal[modulo] + resultado;
-        numeroDecimal = Math.floor(numeroDecimal / base);
+        numeroDecimal = Matºh.floor(numeroDecimal / base);
     }
     return resultado;
 }
 function convertirBases() {
     const numeroDecimal = prompt("Introduce un numero en base 10: ");
-    const hexa = decimalTo(numeroDecimal, 16);
-    const octal = decimalTo(numeroDecimal, 8);
-    const binario = decimalTo(numeroDecimal, 2);
+    const hexa = decimalToBase(numeroDecimal, 16);
+    const octal = decimalToBase(numeroDecimal, 8);
+    const binario = decimalToBase(numeroDecimal, 2);
     document.getElementById("valorDecimal").textContent = `${numeroDecimal} en Decimal:`;
     document.getElementById("resultHexadecimal").textContent = `${hexa} en Hexadecimal`;
     document.getElementById("resultOctal").textContent = `${octal} en Octal`;
