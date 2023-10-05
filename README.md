93423b7f71352907c8c0676e7ad08e0b504a20a7
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-05 17:32:19 +0200

    Ejercicios de la sesion 7 de js acabados

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sesion 7 - Calculadora de Llamadas y Convertidor de Colores Hexadecimales
+
+Esta rama es parte de la "Sesión 7" de ejercicios de JavaScript, que incluye una página web que permite a los usuarios calcular el precio de una llamada telefónica y convertir colores a formato hexadecimal.
+
+## Funcionalidades
+
+- **Calculadora de Llamadas Telefónicas:** Los usuarios pueden ingresar la duración de una llamada en horas, minutos y segundos, y la aplicación calculará el precio de la llamada telefónica. El precio se muestra en euros.
+
+- **Convertidor de Colores a Hexadecimal:** Los usuarios pueden ingresar los valores de rojo, verde y azul de un color en el rango de 0 a 255, y la aplicación convertirá estos valores a su representación hexadecimal (formato RGB hexadecimal). El resultado se muestra en formato "#RRGGBB".
+
+## Cómo Usar
+
+1. Abre el archivo HTML (`index.html`) en tu navegador web.
+
+2. En la sección "Calculadora de Llamadas", haz clic en el botón "Calcular Precio" para ingresar la duración de la llamada telefónica y ver el costo.
+
+3. En la sección "Convertidor de Colores a Hexadecimal", haz clic en el botón "Calcular RGB" para ingresar los valores de color y ver la representación hexadecimal.
+
+## Requisitos
+
+No se requieren dependencias externas para este proyecto. Todo funciona en el navegador web.
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Función para calcular el precio de la llamada telefónica
 function calcularPrecio(horas, minutos, segundos) {
     const precioEstablecimiento = 10; // Precio fijo de establecimiento en céntimos
     const PrecioPorMinuto = 5; // Precio por minuto en céntimos
@@ -10,16 +10,42 @@ function calcularPrecio(horas, minutos, segundos) {
     return precioTotalEuros;
 
 }
+// Función para obtener la duración de la llamada desde el usuario y mostrar el resultado
 function obtenerTiempo() {
     const horas = parseInt(prompt("Introduce las horas:"));
     const minutos = parseInt(prompt("Introduce los minutos:"));
     const segundos = parseInt(prompt("Introduce los segundos:"));
     const precio = calcularPrecio(horas, minutos, segundos);
-    const mensaje = `El precio de la llamada telefonica es ${precio}  €`;
-    const btnPrecio = document.getElementById("msgPrecio");
-    btnPrecio.innerHTML = mensaje;
+    const mensaje = `El precio de la llamada telefonica es ${precio.toFixed(2)} €`;
+    const msgPrecio = document.getElementById("msgPrecio");
+    msgPrecio.innerHTML = mensaje;
 
 
 }
+// Función para obtener el color desde el usuario y mostrar el resultado en formato hexadecimal
+function obtenerColor() {
+    const rojo = parseInt(prompt("Introduce el valor de rojo (0-255):"));
+    const verde = parseInt(prompt("Introduce el valor de verde (0-255):"));
+    const azul = parseInt(prompt("Introduce el valor de azul (0-255):"));
+    // Convertir cada componente a su representación hexadecimal
+    const rojoHex = decimalAHexadecimal(rojo);
+    const verdeHex = decimalAHexadecimal(verde);
+    const azulHex = decimalAHexadecimal(azul);
+    // Crear el formato RGB hexadecimal (#RRGGBB)
+    const colorHex = "#" + rojoHex + verdeHex + azulHex;
+    const mensaje = `En Hexadecimal es: ${colorHex}`;
+    const msgRGB = document.getElementById("msgRGB");
+    msgRGB.innerHTML = mensaje;
+
+
+}
+// Función para convertir un número decimal a hexadecimal
+function decimalAHexadecimal(decimal) {
+    return decimal.toString(16);
+}
+// Obtener los elementos de los botones
 const btnPrecio = document.getElementById("btnPrecio");
 btnPrecio.addEventListener("click", obtenerTiempo)
+
+const btnRGB = document.getElementById("btnRGB");
+btnRGB.addEventListener("click", obtenerColor)
