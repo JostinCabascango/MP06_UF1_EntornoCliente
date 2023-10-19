0fdde2a54b5f097fc25b570603f2d213f18607ec
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-19 11:42:15 +0200

    Refactorizar codigo

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-/*
-    1.crear una tabla de multiplicar
-    2.crear una piramide de colores
- */
+// Función para crear una tabla de multiplicar
 function crearTablaDeMultiplicar(filas, columnas) {
     const tablaMultiplicarDiv = document.getElementById("tablaMultiplicar");
-
     tablaMultiplicarDiv.innerHTML = "";
-
-    const tabla = document.createElement("table");
-    tabla.style.width = "100%";
-    tabla.style.margin = "1rem";
-    tabla.style.padding = "1rem";
+    const tabla = crearTabla();
 
     for (let i = 1; i <= filas; i++) {
-        const fila = document.createElement("tr");
+        const fila = crearFila();
 
         for (let j = 1; j <= columnas; j++) {
-            const celda = document.createElement("td");
-            celda.style.border = "2px solid black";
-            celda.style.padding = "0.5rem";
-            celda.style.textAlign = "center";
-
-            const resultado = document.createTextNode(`${i * j}`);
-            celda.appendChild(resultado);
+            const celda = crearCelda();
+            celda.textContent = i * j;
             fila.appendChild(celda);
         }
 
@@ -30,10 +17,70 @@ function crearTablaDeMultiplicar(filas, columnas) {
     }
 
     tablaMultiplicarDiv.appendChild(tabla);
+}
+
+// Función para generar una pirámide de colores
+function generarPiramide(numColumnas) {
+    const piramideDiv = document.getElementById("tablaPiramide");
+    piramideDiv.innerHTML = "";
+    const tabla = crearTabla();
+    const numFilas = Math.floor(numColumnas / 2) + 1; // El número de filas es la mitad de las columnas más 1 para que sea entero el número de filas
+    let numBlancos = Math.floor((numColumnas - 1) / 2); // Numero de celdas blancas en la primera fila de la pirámide (la mitad de las columnas menos 1)
+    let numRojos = 1; // Inicializamos el número de celdas rojas en 1 para la primera fila de la pirámide
+
+    for (let i = 0; i < numFilas; i++) {
+        const fila = crearFila();
+
+        agregarCeldas(fila, numBlancos, "white");
+        agregarCeldas(fila, numRojos, "red");
+        agregarCeldas(fila, numBlancos, "white");
+
+        numRojos += 2; // Aumentamos el número de celdas rojas en 2 para la siguiente fila de la pirámide
+        numBlancos--; // Disminuimos el número de celdas blancas en 1 para la siguiente fila de la pirámide
+
+        tabla.appendChild(fila);
+    }
+
+    piramideDiv.appendChild(tabla);
+}
+
+// Función para crear una tabla HTML con estilos CSS
+function crearTabla() {
+    const tabla = document.createElement("table");
+    tabla.style.borderCollapse = "collapse";
+    tabla.style.border = "1px solid black";
+    tabla.style.margin = "0.5rem";
+    tabla.style.padding = "0.5rem";
+    tabla.style.width = "100%";
     return tabla;
 }
 
+// Función para crear una fila HTML
+function crearFila() {
+    return document.createElement("tr");
+}
+
+// Función para crear una celda HTML Con estilos CSS
+function crearCelda() {
+    const celda = document.createElement("td");
+    celda.style.backgroundColor = "white";
+    celda.style.width = "20px";
+    celda.style.height = "20px";
+    celda.style.border = "3px solid black";
+    return celda;
+}
+
+// Función para agregar celdas a una fila con un color determinado
+function agregarCeldas(fila, numCeldas, color) {
+    for (let i = 0; i < numCeldas; i++) {
+        const celda = crearCelda();
+        celda.style.backgroundColor = color;
+        fila.appendChild(celda);
+    }
+}
+// Llamada a las funciones para crear la tabla de multiplicar y la pirámide
 const btnGenerarTablaMultiplicar = document.getElementById("btnGenerarTablaMultiplicar");
+const btnGenerarPiramide = document.getElementById("btnGenerarPiramide");
 
 btnGenerarTablaMultiplicar.addEventListener("click", function () {
     const filas = parseInt(prompt("Introduce el número de filas"));
@@ -42,44 +89,7 @@ btnGenerarTablaMultiplicar.addEventListener("click", function () {
     crearTablaDeMultiplicar(filas, columnas);
 });
 
-const btnGenerarPiramide = document.getElementById("btnGenerarPiramide");
-
 btnGenerarPiramide.addEventListener("click", function () {
-    const columnasColor = parseInt(prompt("Introduce el número de columnas de color"));
-    const piramideDiv = document.getElementById("tablaPiramide");
-
-    piramideDiv.innerHTML = "";
-
-    generarPiramide(columnasColor, piramideDiv);
+    const columnasColor = parseInt(prompt("Introduce el número de columnas de color para la pirámide"));
+    generarPiramide(columnasColor);
 });
-
-function generarPiramide(numColumnas, piramideDiv) {
-    const tabla = document.createElement("table");
-    tabla.style.width = "100%";
-    tabla.style.margin = "1rem";
-    tabla.style.padding = "1rem";
-
-    for (let i = numColumnas; i >= 1; i--) {
-        const fila = document.createElement("tr");
-
-        for (let j = 1; j <= numColumnas; j++) {
-            const celda = document.createElement("td");
-            celda.style.width = "20px";
-            celda.style.height = "20px";
-            celda.style.backgroundColor = "white";
-            celda.style.border = "2px solid black";
-            celda.style.padding = "0.5rem";
-
-            if (j >= i && j <= numColumnas - i + 1) {
-                celda.style.backgroundColor = "red";
-            }
-
-            fila.appendChild(celda);
-        }
-
-        tabla.appendChild(fila);
-    }
-
-    piramideDiv.appendChild(tabla);
-    return tabla;
-}
